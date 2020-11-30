505573d70c080ba6fd6caab7f7182557f6a2d9cd
David-Souza-Batista <75282329+David-Souza-Batista@users.noreply.github.com>
2020-11-30 20:32:24 -0300

    files
    arquivos usados

---
+++ cadastro.php
@@ -0,0 +1,21 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <title>Cadastro - Instituto Luz aos Cegos</title>
+    <link rel="stylesheet" href="css/style.css">    
+</head>
+<body>
+   <div id="form-cad"> 
+    <h1>Cadastrar</h1>
+    <from method="POST" action="processa.php">
+        <input type="text" placeholder="Nome completo">
+        <input type="text" placeholder="Telefone">
+        <input type="email" placeholder="Usuário">
+        <input type="password" placeholder="Senha">    
+        <input type="password" placeholder="Confirme a senha">    
+        <input type="submit" value="Entrar">     
+        <a href="login.php">Já possui login?<strong>Entre<strong> </a>
+    </form>
+</div> 
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <title>Login - Instituto Luz aos Cegos</title>
+    <link rel="stylesheet" href="css/style.css">    
+</head>
+<body>
+   <div id="form"> 
+    <h1>Entrar</h1>
+    <from method="POST" action="processa.php">
+        <input type="email" placeholder="Usuário">
+        <input type="password" placeholder="Senha">    
+        <input type="submit" value="Entrar" href="administrativa.html">     
+        <a href="cadastro.php">não possui login?<strong>Cadastre-se<strong> </a>
+    </form>
+</div> 
+</body>
+</html>
